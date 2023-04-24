012072e6d2f417ba7fa4af6127a10de567e15f78
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2023-04-24 12:51:33 +0400

    precompiled header update

--- Vortex/headers/vtpch.hpp
+++ Vortex/headers/vtpch.hpp
@@ -39,7 +39,12 @@
 
 // GLM
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp> // glm::pi
 
 #ifdef VT_PLATFORM_WIN
 	#include <Windows.h>
