9debd965425c2b9cf51e20ad084f99d3a3846993
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2023-04-29 23:27:08 +0400

    added platform checking

--- Vortex/headers/Vortex/Core/Core.hpp
+++ Vortex/headers/Vortex/Core/Core.hpp
@@ -2,6 +2,64 @@
 
 #include <memory>
 
+#ifdef _WIN32
+/* Windows x64/x86 */
+	#ifdef _WIN64
+			/* Windows x64  */
+		#define VT_PLATFORM_WIN
+	#else
+			/* Windows x86 */
+		#error "x86 Builds are not supported!"
+	#endif
+
+#elif defined(__APPLE__) || defined(__MACH__)
+	#include <TargetConditionals.h>
+	/* TARGET_OS_MAC exists on all the platforms
+	 * so we must check all of them (in this order)
+	 * to ensure that we're running on MAC
+	 * and not some other Apple platform */
+	#if TARGET_IPHONE_SIMULATOR == 1
+		#error "IOS simulator is not supported!"
+	#elif TARGET_OS_IPHONE == 1
+		#define VT_PLATFORM_IOS
+		#error "IOS is not supported!"
+	#elif TARGET_OS_MAC == 1
+		#define VT_PLATFORM_MACOS
+		#error "MacOS is not supported!"
+	#else
+		#define VORTEX_API __declspec(dllimport)
+		#error "Unknown Apple platform!"
+	#endif
+	 /* We also have to check __ANDROID__ before __linux__
+	  * since android is based on the linux kernel
+	  * it has __linux__ defined */
+#elif defined(__ANDROID__)
+	#define VT_PLATFORM_ANDROID
+	#error "Android is not supported!"
+#elif defined(__linux__)
+	#define VT_PLATFORM_LINUX
+	#error "Linux is not supported!"
+#else
+	#define VORTEX_API
+	/* Unknown compiler/platform */
+	#error "Unknown platform!"
+#endif
+
+// DLL support
+#ifdef VT_PLATFORM_WIN
+	#ifdef VT_DYNAMIC_LINK
+		#ifdef VT_BUILD_DLL
+			#define VORTEX_API __declspec(dllexport)
+		#else
+			#define VORTEX_API __declspec(dllimport)
+		#endif
+	#else
+		#define VORTEX_API
+	#endif
+#else
+	#error Vortex only supports Windows!
+#endif
+
 #ifdef VT_PLATFORM_WIN
 	#ifdef VT_ENABLE_ASSERTS
 		#define VT_CORE_ASSERT(x, ...) { if(!(x)) { VT_CORE_ERROR("Assertion failed {0}", __VA_ARGS__); __debugbreak(); } }
