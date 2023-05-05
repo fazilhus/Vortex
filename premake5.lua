69e6fc7d4c6341f5703404b3a8fd97186500b38f
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2023-05-05 20:37:29 +0400

    cleaned up the solution dir

--- premake5.lua
+++ premake5.lua
@@ -65,6 +65,10 @@ project "Vortex-Editor"
         "Vortex"
     }
 
+    prebuildcommands {
+        "{COPY} ../bin/" .. outputdir .. "/Vortex/*.lib %{cfg.buildtarget.directory}"
+    }
+
     filter "system:windows"
         systemversion "latest"
 
@@ -148,59 +152,6 @@ project "Vortex"
         defines {
             "VT_DEBUG",
             "VT_ENABLE_ASSERTS",
-            "VT_PROFILE"
-        }
-        runtime "Debug"
-        symbols "on"
-
-    filter "configurations:Release"
-        defines "VT_RELEASE"
-        runtime "Release"
-        optimize "on"
-
-    filter "configurations:Dist"
-        defines "VT_DIST"
-        runtime "Release"
-        optimize "on"
-
-project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language (lang)
-    cppdialect (cppdlct)
-    staticruntime "on"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files {
-        "%{prj.name}/headers/**.hpp",
-        "%{prj.name}/sources/**.cpp",
-        "Vortex/vendor/stb/**.h",
-        "Vortex/vendor/stb/**.cpp"
-    }
-
-    includedirs {
-        "%{prj.name}/headers",
-        "Vortex/headers",
-        "%{includedir.spdlog}",
-        "%{includedir.glfw}",
-        "%{includedir.glad}",
-        "%{includedir.imgui}",
-        "%{includedir.glm}",
-        "%{includedir.stb}"
-    }
-
-    links {
-        "Vortex"
-    }
-
-    filter "system:windows"
-        systemversion "latest"
-
-    filter "configurations:Debug"
-        defines {
-            "VT_DEBUG",
             "VT_PROFILE"
         }
         runtime "Debug"
